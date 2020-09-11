5bc58f240a804594bb43441565e846034797389a
Choonham <dog026@naver.com>
2020-09-11 11:04:39 +0900

    Add Skill "Flash"

--- Skills.java
+++ Skills.java
@@ -50,7 +50,7 @@ public class Skills implements Listener {
         //************각 직업별 10랩 스킬
         if ((action.equals(Action.LEFT_CLICK_BLOCK)||(action.equals(Action.LEFT_CLICK_AIR)))
                 && (p.getItemInHand().getType().equals(Material.COAL))){
-            if(Class.equals("Arc") && LV > 10){
+            if(Class.equals("Arc") && LV > 1){
                 if(ArcSkill1CoolTime.containsKey(p.getName())){
                     if(ArcSkill1CoolTime.get(p.getName())>System.currentTimeMillis()) {
                         long timeleft = (ArcSkill1CoolTime.get(p.getName()) - System.currentTimeMillis())/1000;
@@ -64,19 +64,19 @@ public class Skills implements Listener {
             int y = b.getY();
             int z = b.getZ();
             for(int i = 0; i < 4; i++){
-                for(int j = 0; j < 4; j++) {
-                    Block w1 = currentWorld.getBlockAt(x - 2 + i, y+j, z - 2);
-                    Block w2 = currentWorld.getBlockAt(x - 2 + i, y+j, z + 2);
-                    Block w3 = currentWorld.getBlockAt(x - 2, y+j, z - 2 + i);
-                    Block w4 = currentWorld.getBlockAt(x + 2, y+j, z - 2 + i);
-                    w1.setType(Material.IRON_BLOCK);
-                    w2.setType(Material.IRON_BLOCK);
-                    w3.setType(Material.IRON_BLOCK);
-                    w4.setType(Material.IRON_BLOCK);
-                }
+                    for(int j = 0; j < 4; j++) {
+                        Block w1 = currentWorld.getBlockAt(x - 2 + i, y+j, z - 2);
+                        Block w2 = currentWorld.getBlockAt(x - 2 + i, y+j, z + 2);
+                        Block w3 = currentWorld.getBlockAt(x - 2, y+j, z - 2 + i);
+                        Block w4 = currentWorld.getBlockAt(x + 2, y+j, z - 2 + i);
+                        w1.setType(Material.IRON_BLOCK);
+                        w2.setType(Material.IRON_BLOCK);
+                        w3.setType(Material.IRON_BLOCK);
+                        w4.setType(Material.IRON_BLOCK);
+                    }
                 }
             }
-            if(Class.equals("hunter") && LV > 10){
+            if(Class.equals("hunter") && LV > 1){
                 if(hunterSkill1CoolTime.containsKey(p.getName())){
                     if(hunterSkill1CoolTime.get(p.getName())>System.currentTimeMillis()) {
                         long timeleft = (hunterSkill1CoolTime.get(p.getName()) - System.currentTimeMillis())/1000;
@@ -102,8 +102,8 @@ public class Skills implements Listener {
         //************각 직업별 10랩 스킬
 
         //************Flash********//
-        if (action.equals(Action.LEFT_CLICK_BLOCK)||(action.equals(Action.LEFT_CLICK_AIR))
-                && p.getItemInHand().getType().equals(Material.GOLDEN_CARROT)){
+        if ((action.equals(Action.LEFT_CLICK_BLOCK)||(action.equals(Action.LEFT_CLICK_AIR)))
+                && (p.getItemInHand().getType().equals(Material.GOLDEN_CARROT))){
             if(FlashSkill3CoolTime.containsKey(p.getName())){
                 if(FlashSkill3CoolTime.get(p.getName())>System.currentTimeMillis()) {
                     long timeleft = (FlashSkill3CoolTime.get(p.getName()) - System.currentTimeMillis())/1000;
@@ -111,21 +111,91 @@ public class Skills implements Listener {
                     return;
                 }
             }
-            FlashSkill3CoolTime.put(p.getName(), System.currentTimeMillis() + (1*1000));
-            Block b = p.getTargetBlock(null,3);
-
+            FlashSkill3CoolTime.put(p.getName(), System.currentTimeMillis() + (30*1000));
+            String Dir = rpGetPlayerDirection(p);
             Location pLoc = p.getLocation();
-            Location bLoc = b.getLocation();
+            double pX = pLoc.getX();
+            double pY = pLoc.getY();
+            double pZ = pLoc.getZ();
+            float yaw = (float)pLoc.getYaw();
+            if(Dir.equals("west")) {
+                Location Flash = new Location(p.getWorld(), pX, pY, pZ + 6, yaw, 1);
+                p.teleport(Flash);
 
-            int px = pLoc.getBlockX();
-            int py = pLoc.getBlockY();
-            int pz = pLoc.getBlockZ();
-
-            int bx = bLoc.getBlockX();
-            int by = bLoc.getBlockY();
-            int bz = bLoc.getBlockZ();
-
+            } else if(Dir.equals("west northwest")){
+                Location Flash = new Location(p.getWorld(), pX - 3, pY, pZ + 6, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("northwest")){
+                Location Flash = new Location(p.getWorld(), pX - 6, pY, pZ + 6, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("north northwest")){
+                Location Flash = new Location(p.getWorld(), pX - 6, pY, pZ + 3, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("north")){
+                Location Flash = new Location(p.getWorld(), pX - 6, pY, pZ, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("north northeast")){
+                Location Flash = new Location(p.getWorld(), pX - 6, pY, pZ -3, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("northeast")){
+                Location Flash = new Location(p.getWorld(), pX - 6, pY, pZ -6, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("east northeast")){
+                Location Flash = new Location(p.getWorld(), pX -3, pY, pZ - 6, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("east")){
+                Location Flash = new Location(p.getWorld(), pX, pY, pZ - 6, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("east southeast")){
+                Location Flash = new Location(p.getWorld(), pX + 3, pY, pZ - 6, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("southeast")){
+                Location Flash = new Location(p.getWorld(), pX + 6, pY, pZ - 6, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("south southeast")){
+                Location Flash = new Location(p.getWorld(), pX + 6, pY, pZ - 3, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("south")){
+                Location Flash = new Location(p.getWorld(), pX + 6, pY, pZ, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("south southwest")){
+                Location Flash = new Location(p.getWorld(), pX + 6, pY, pZ + 3, yaw, 1);
+                p.teleport(Flash);
+            } else if(Dir.equals("southwest")){
+                Location Flash = new Location(p.getWorld(), pX + 6, pY, pZ + 6, yaw, 1);
+                p.teleport(Flash);
+            } else {
+                Location Flash = new Location(p.getWorld(), pX + 3, pY, pZ + 6, yaw, 1);
+               p.teleport(Flash);;
+            }
+            }
         }
-        //************Flash********//
+
+
+    public String rpGetPlayerDirection(Player playerSelf) {
+        String dir = "";
+        float y = playerSelf.getLocation().getYaw();
+        if( y < 0 ){y += 360;}
+        y %= 360;
+        int i = (int)((y+8) / 22.5);
+        if(i == 0){dir = "west";}
+        else if(i == 1){dir = "west northwest";}
+        else if(i == 2){dir = "northwest";}
+        else if(i == 3){dir = "north northwest";}
+        else if(i == 4){dir = "north";}
+        else if(i == 5){dir = "north northeast";}
+        else if(i == 6){dir = "northeast";}
+        else if(i == 7){dir = "east northeast";}
+        else if(i == 8){dir = "east";}
+        else if(i == 9){dir = "east southeast";}
+        else if(i == 10){dir = "southeast";}
+        else if(i == 11){dir = "south southeast";}
+        else if(i == 12){dir = "south";}
+        else if(i == 13){dir = "south southwest";}
+        else if(i == 14){dir = "southwest";}
+        else if(i == 15){dir = "west southwest";}
+        else {dir = "west";}
+        return dir;
     }
+        //************Flash********//
 }
