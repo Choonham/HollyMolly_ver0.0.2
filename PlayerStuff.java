5f7b4acc34fb10895ee91f8d16d03064bc569e69
Choonham <dog026@naver.com>
2020-09-17 16:12:02 +0900

    You get 10 poos when you die.

--- PlayerStuff.java
+++ PlayerStuff.java
@@ -1,16 +1,21 @@
 package com.jun.hollymolly;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 import java.util.HashMap;
 import java.util.Random;
 //HollyMollyPlugIn ver.0.0.2(latest release on 2020.09.15)
 public class PlayerStuff implements Listener {
     public HashMap<Player, ItemStack[]> items = new HashMap<>();
+
 
     @EventHandler
     public void onRespawn(PlayerRespawnEvent event) {
@@ -27,8 +32,18 @@ public class PlayerStuff implements Listener {
 
     @EventHandler()
     public void onDeath(PlayerDeathEvent e) {
-        e.getEntity();
-        ItemStack[] content = e.getEntity().getInventory().getContents();
+        ItemStack[] content;
+        for(int i = 0; i<=10; i++){
+            Random Rand = new Random();
+            int RandInt = Rand.nextInt(40);
+            ItemStack Item1 = new ItemStack(Material.DIRT);
+            ItemMeta meta = Item1.getItemMeta();
+            meta.setDisplayName(ChatColor.GOLD + "똥" + i);
+            Item1.setItemMeta(meta);
+            e.getEntity().getInventory().setItem(RandInt, Item1);
+        }
+
+        content = e.getEntity().getInventory().getContents();
         items.put(e.getEntity(), content);
         e.getDrops().clear();
     }
